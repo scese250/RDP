31ba4c86ccd1cf6b8edcfe394c2a118d4df484dd
scese250 <cuentas1912@gmail.com>
2021-01-27 15:22:37 -0400

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -30,7 +30,7 @@ printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
 su - RDP -c """$CRP"""
-printf 'Check https://remotedesktop.google.com/access/ \n\n'
+printf 'Check https://remotedesktop.google.com/u/1/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
     printf "\n\nUpgrade Completed " >&2
